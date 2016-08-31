c084ced1f8b4134c04b3e5da1d1a63ee217919d6
gabescarbrough <gabescarbrough@gmail.com>
2016-08-31 16:06:30 -0400

    Create script to upload file to AWS
    Wrote Node script to upload a file and its title to AWS SS3 from command line

---
+++ bin/aws-s3-upload.js
@@ -0,0 +1,24 @@
+'use strict';
+
+require('dotenv').load();
+
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const fs = require('fs');
+
+let file = {
+  path: process.argv[2],
+  title: process.argv[3],
+};
+
+let stream = fs.createReadStream(file.path);
+
+const params = {
+  Bucket: process.env.AWS_S3_BUCKET_NAME,
+  Key: 'key1',
+  Body: stream,
+};
+
+s3.upload(params, function(err, data) {
+  console.log(err, data);
+});
