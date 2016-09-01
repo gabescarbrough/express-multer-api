eb4fcd11790aacab58173cb4e7067b18c3adb5f9
gabescarbrough <gabescarbrough@gmail.com>
2016-09-01 15:27:59 -0400

    Complete API upload endpoint
    Create action stores file on S3 and adds document to mongodb

--- config/routes.js
+++ config/routes.js
@@ -10,6 +10,9 @@ module.exports = require('lib/wiring/routes')
 // standards RESTful routes
 .resources('examples')
 
+.get('/uploads', 'uploads#index')
+.post('/uploads', 'uploads#create')
+
 // users of the app have special requirements
 .post('/sign-up', 'users#signup')
 .post('/sign-in', 'users#signin')
