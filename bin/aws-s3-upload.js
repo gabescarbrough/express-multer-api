409008fe05f9a3e0614352f9e56ecad44d622442
gabescarbrough <gabescarbrough@gmail.com>
2016-09-01 09:45:23 -0400

    Add extension to S3 `Key` for uploaded files
    Chrome not displaying images without extensions.

--- bin/aws-s3-upload.js
+++ bin/aws-s3-upload.js
@@ -5,6 +5,9 @@ require('dotenv').load();
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const fs = require('fs');
+const path = require('path');
+
+const mime = require('mime');
 
 let file = {
   path: process.argv[2],
@@ -12,11 +15,14 @@ let file = {
 };
 
 let stream = fs.createReadStream(file.path);
+let contentType = mime.lookup(file.path);
+let ext = path.extname(file.path);
 
 const params = {
   ACL: 'public-read',
   Bucket: process.env.AWS_S3_BUCKET_NAME,
-  Key: 'key1',
+  ContentType: contentType,
+  Key: 'key1' + ext,
   Body: stream,
 };
 
