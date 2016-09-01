7d6d1a19106c015214598b2809fe88e289cf01e3
gabescarbrough <gabescarbrough@gmail.com>
2016-09-01 13:39:26 -0400

    Add Upload model; use in command line script
    Model stores url and title of image uploaded to S3

--- bin/aws-s3-upload.js
+++ bin/aws-s3-upload.js
@@ -4,11 +4,23 @@ require('dotenv').load();
 
 const s3Upload = require('../lib/aws-s3-upload');
 
+const mongoose = require('../app/middleware/mongoose');
+
+const Upload = require('../app/models/upload');
+
+
+
 let file = {
   path: process.argv[2],
   title: process.argv[3],
 };
 
 s3Upload(file)
-  .then(data => console.log(data))
-  .catch(err => console.error(err));
+  .then((s3response) =>
+    Upload.create({
+      url: s3response.Location,
+      title: file.title,
+    }))
+  .then((data) => console.log(data))
+  .catch((err) => console.error(err))
+  .then(() => mongoose.connection.close());
