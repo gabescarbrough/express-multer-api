614027d1df0df8cf859895e9a9685894fc2f3eaa
gabescarbrough <gabescarbrough@gmail.com>
2016-09-01 15:06:56 -0400

    Refactor s3Upload function
    Parallel multer file object
    Ease testing of s3Upload and Upload.create

--- bin/aws-s3-upload.js
+++ bin/aws-s3-upload.js
@@ -4,16 +4,18 @@ require('dotenv').load();
 
 const s3Upload = require('../lib/aws-s3-upload');
 
+const mime = require('mime');
+
 const mongoose = require('../app/middleware/mongoose');
-
 const Upload = require('../app/models/upload');
-
-
 
 let file = {
   path: process.argv[2],
   title: process.argv[3],
 };
+
+file.mimetype = mime.lookup(file.path);
+file.originalname = file.path;
 
 s3Upload(file)
   .then((s3response) =>
